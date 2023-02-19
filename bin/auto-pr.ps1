7d4ff87483204f27d39ea53154e8f0ac77fdfc25
windedge <windedge99@gmail.com>
2023-02-19 01:10:52 +0000

    fix: incorrect branch

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "windedge/ladle-bucket:main"
+    [String]$upstream = "windedge/ladle-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
